f6dee69c724356aa390359e376183a5f5d9d2718
zhan2736 <zhan2736@umn.edu>
2015-07-07 14:03:51 -0500

    mmrsim

---
+++ mmrsim/MmrSim.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmrsim;
+
+/**
+ *
+ * @author zenbhang
+ */
+public class MmrSim {
+/**
+ * @MMRSimulator
+ *  See the readme for introduction and how it works
+ * 
+ * 
+ */
+    
+    public static void main(String[] args) {
+        gui GUI = new gui();
+        GUI.setVisible(true);
+        
+    }
+    
+}
